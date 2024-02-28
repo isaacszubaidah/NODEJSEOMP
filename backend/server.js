a28e2c9cb9f70c28e607d509722b8184715ad28b
Asithandile Tyulu <140261777+AsithandileNyjah@users.noreply.github.com>
2024-02-28 09:23:02 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,12 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://nodejseomp-1.onrender.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE");
+    next();
+});
 
 app.use(cors(corsOptions));
 
